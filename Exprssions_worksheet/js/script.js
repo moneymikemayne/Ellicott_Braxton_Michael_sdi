2ee34f7123cf919333fcc284658fb111fb29cba0
moneymikemayne <mellicott25@gmail.com>
2015-09-10 11:25:06 -0400

    expressions assignment

---
+++ Exprssions_worksheet/js/script.js
@@ -0,0 +1,12 @@
+//Class notes
+var yearBorn = 1955;
+
+alert("hello world");
+alert(yearBorn);
+
+//var x desribes the number of dog years var y describes the number of human years
+var x = 7;
+ var y = 1;
+//the formula is used to dertime the number of dog years in relation to human years
+console.log(x + y)
+
