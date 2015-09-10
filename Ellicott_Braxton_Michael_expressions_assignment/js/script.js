f66d83b6f1132567e2a312875a5283d96f072886
moneymikemayne <mellicott25@gmail.com>
2015-09-10 19:14:08 -0400

    expressions assignment

--- Ellicott_Braxton_Michael_expressions_assignment/js/script.js
+++ Ellicott_Braxton_Michael_expressions_assignment/js/script.js
@@ -1,9 +1,15 @@
 // The are of a rectangular prism = 2ab+2bc+2ac
 
 var sideOne = 2; //side one of the rectangle
-var sideTwo = 3; //side 2 of the rectangle
+
+var sideTwo = 3; //side two of the rectangle
+
 var sideThree = 4; //side 3 of the rectangle
 
+// var area represents the equation to get the area of a rectangular prism
 var area = (2 * sideOne * sideTwo) + (2 * sideTwo * sideThree) + (2 * sideOne * sideThree);
 
 console.log(area);
+
+
+
