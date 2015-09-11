3946272bf71b3d3167f0a3cbe058ff3ce87a6d03
moneymikemayne <mellicott25@gmail.com>
2015-09-10 20:44:17 -0400

    still practice

--- practice/js/myscript.js
+++ practice/js/myscript.js
@@ -1,15 +1,15 @@
-// The are of a rectangular prism = 2ab+2bc+2ac
-
-var sideOne = 2; //side one of the rectangle
-
-var sideTwo = 3; //side two of the rectangle
-
-var sideThree = 4; //side 3 of the rectangle
-
-// var area represents the equation to get the area of a rectangular prism
-var area = (2 * sideOne * sideTwo) + (2 * sideTwo * sideThree) + (2 * sideOne * sideThree);
-
-console.log(area);
 
 
 
+    var a = 2
+
+    var b  = 3
+
+    var c = 4
+
+    var area = ((2*a*b) + (2*b*c) + (2*a*c))
+
+    var units = ["in", "ft", "mi", "cm", "m", "Km"]
+
+
+<button type="button" onclick="document.write(area " + "measurement[1])">Try it </button>
