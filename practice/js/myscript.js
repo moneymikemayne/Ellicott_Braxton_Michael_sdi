7a88da445438edb1ffb9e1300bbea4aee8e2d93e
moneymikemayne <mellicott25@gmail.com>
2015-09-10 21:15:20 -0400

    getting closer

--- practice/js/myscript.js
+++ practice/js/myscript.js
@@ -1,15 +1,4 @@
 
 
 
-    var a = 2
 
-    var b  = 3
-
-    var c = 4
-
-    var area = ((2*a*b) + (2*b*c) + (2*a*c))
-
-    var units = ["in", "ft", "mi", "cm", "m", "Km"]
-
-
-<button type="button" onclick="document.write(area " + "measurement[1])">Try it </button>
