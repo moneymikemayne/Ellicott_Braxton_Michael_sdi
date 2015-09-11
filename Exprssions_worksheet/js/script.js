d87ffd6f116861347fa0494bc1424c42983cabba
moneymikemayne <mellicott25@gmail.com>
2015-09-10 20:03:53 -0400

    practice

---
+++ Exprssions_worksheet/js/script.js
@@ -0,0 +1,18 @@
+document.write("<h1>The </h1>");
+// var a is equivalent to the length of side a
+var a= document.write(prompt("Enter the length of side one : ","numb"));
+
+// var b is equivalent to the length of side b
+var b= document.write(prompt("Enter the length of side two: ", "numb"));
+
+// var c is equivalent to the length of side c
+var c= document.write(prompt("Enter the length of side three: ", "numb"));
+
+// the formula of a rectangular prism
+var area = window.alert (2*a*b)+(2*b*c)+(2*a*c)+ measurement[0];
+
+//array for units of measurement
+var measurement = ["in", "ft","mm","cm", "m"]
+
+
+    <button type="button" onclick="document.write(area)">Try it</button>
